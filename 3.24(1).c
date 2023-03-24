f7e90213749306085b442701beb590a72edec709
Stack0verFl0w7 <2732226915@qq.com>
2023-03-24 20:34:04 +0800

    1

---
+++ 3.24(1).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void test1(int arr[])
+{
+	printf("%d\n", sizeof(arr));
+}
+void test2(char ch[])
+{
+	printf("%d\n", sizeof(ch));
+}
+
+int main()
+{
+	int arr[10] = { 0 };
+	char ch[10] = { 0 };
+	printf("%d\n", sizeof(arr));//40
+	printf("%d\n", sizeof(ch));//10
+	test1(arr);//8-÷∏’Î¥Û–°
+	test2(ch);//8
+	return 0;
+}
