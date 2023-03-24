412f1b2f4ccb152ff02c83e5effd84f3883c89e1
Stack0verFl0w7 <2732226915@qq.com>
2023-03-24 20:49:02 +0800

    3.||

---
+++ 3.24(3).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int i = 0, a = 0, b = 2, c = 3, d = 4;
+	i = a++ || ++b || d++;
+	printf("a=%d\n b=%d\n c=%d\n d=%d\n", a, b, c, d);
+	//a=1 b=2 c=3 d=4
+	//a++执行，++b和d++没有执行
+	//||如果左边为真，则右边不计算
+	return 0;
+}
