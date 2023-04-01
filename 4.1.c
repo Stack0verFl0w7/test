6ce8914038f7af10d26da55a952f3980edadce11
Stack0verFl0w7 <2732226915@qq.com>
2023-04-01 21:11:32 +0800

    字节序

---
+++ 4.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int check_sys()
+{
+	int a = 1;
+	char* p = (char*)&a;
+	if (*p == 1)
+		return 1;
+	else
+		return 0;
+}
+
+int main()
+{
+	int ret = check_sys();
+	if (ret = 1)
+	{
+		printf("Ð¡¶Ë\n");
+	}
+	else
+	{
+		printf("´ó¶Ë\n");
+	}
+	return 0;
+}
