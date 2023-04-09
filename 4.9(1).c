bc36ce08df9c5d3b3b2f642dc2fa5d00356ad398
Stack0verFl0w7 <2732226915@qq.com>
2023-04-09 19:47:30 +0800

    test

---
+++ 4.9(1).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	char arr[] = "abcdef";
+	char* pc = arr;
+	printf("%s\n", arr);
+	printf("%s\n", pc);
+	return 0;
+}
---
+++ 4.9(2).c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	char* p = "abcdef";//"abcdef"是一个常量字符串
+	//printf("%c\n",*p);
+	printf("%s\n", p);
+	return 0;
+}
