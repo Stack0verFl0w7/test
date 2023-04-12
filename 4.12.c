b8d5b3e1c0052a733e42b1bf340e317b8c8a952e
Stack0verFl0w7 <2732226915@qq.com>
2023-04-12 21:31:28 +0800

    test

---
+++ 4.12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+struct S1
+{
+	char c1;
+	int a;
+	char c2;
+};
+
+struct S2
+{
+	char c1;
+	char c2;
+	int a;
+};
+
+int main()
+{
+	struct S1 s1 = { 0 };
+	printf("%d\n", sizeof(s1));
+	struct S2 s2 = { 0 };
+	printf("%d\n", sizeof(s2));
+	return 0;
+}
