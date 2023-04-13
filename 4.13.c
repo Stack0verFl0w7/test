e5356a468d3dafa30d450006c1e0caaaf53befc9
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-04-13 21:46:54 +0800

    Update 4.13.c

--- 4.13.c
+++ 4.13.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 struct s1
 {
-	double d;
-	char c;
-	int i;
+	double d;//8
+	char c;//1
+	int i;//4
 };
 
 struct s2
 {
-	char c1;
-	struct s1 s1;
-	double d;
+	char c1;//1
+	struct s1 s1;//16
+	double d;//8
 };
 
 int main()
 {
-	printf("%d\n", sizeof(struct s1));//16
-	printf("%d\n", sizeof(struct s2));//32
+	printf("%d\n", sizeof(struct s1));//16-8+1+(3)+4
+	printf("%d\n", sizeof(struct s2));//32-1+(7)+16+8
 	return 0;
 }
