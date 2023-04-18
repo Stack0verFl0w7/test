6c9e77e9fc2ef14ba95e1772e8fd889a80fd2498
Stack0verFl0w7 <2732226915@qq.com>
2023-04-18 20:31:58 +0800

    test

---
+++ 4.18.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+struct S
+{
+	char c;
+	int i;
+	double d;
+};
+
+void Init(struct S* ps)
+{
+	ps->i = 100;
+	ps->c = 'w';
+	ps->d = 3.14;
+}
+
+void Print1(struct S tmp)
+{
+	printf("%d %c %lf ", tmp.i, tmp.c, tmp.d);
+}
+
+void Print2(struct S* ps)
+{
+	printf("%d %c %lf ", ps->i, ps->c, ps->d);
+}
+
+int main()
+{
+	struct S s = { 0 };
+	Init(&s);
+	Print1(s);
+	Print2(&s);
+	return 0;
+}
