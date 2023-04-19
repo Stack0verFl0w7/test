0df51e148aca50bd079be5e827ad69409e2a08bf
Stack0verFl0w7 <2732226915@qq.com>
2023-04-19 19:16:46 +0800

    练习第1题

---
+++ 第1题.c
@@ -0,0 +1,16 @@
+//第一题
+//题目描述：
+//  于老师经常告诉我们学习编程最好的办法是上机实践，
+//  因为要对计算机下指令，想让计算机帮忙干活就得多和计算机“交流”，实践才能出真知。
+//输入描述：
+//  无
+//输出描述：
+//  Practice makes perfect!
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Practice makes perfect!");
+	return 0;
+}
