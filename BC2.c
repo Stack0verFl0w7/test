5606e7290388214ea0a231c69a9d552c928ff71a
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-04-20 17:33:51 +0800

    Update BC2.c

--- BC2.c
+++ BC2.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main() {
-    printf("     **     \n     **     \n************\n************\n    *  *    \n    *  *    \n");
-    return 0;
+int main()
+{
+	printf("     **     \n");
+	printf("     **     \n");
+	printf("************\n");
+	printf("************\n");
+	printf("    *  *    \n");
+	printf("    *  *    \n");
+	return 0;
 }
