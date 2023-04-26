2f0ce8da43c3ddb00ba44a397772a9bf383f99e4
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-04-26 21:57:04 +0800

    Update BC8.c

--- BC8.c
+++ BC8.c
@@ -6,15 +6,28 @@
 //输出一个用这个字符填充的对角线长5个字符，倾斜放置的菱形
 
 #include <stdio.h>
+#include<math.h>
 
 int main() {
 	char a;
-	int i;
-	int j;
+	int i1; //上3行
+	int i2; //下2行
+	int j;  //列
 	scanf("%c", &a);
-	for (i = 1; i <= 5; i++) {
-		for (j = 1; j <= 5; j++) {
-			if (i == 3 || j == 3 || (i == 2 && ((j == 4) || (j == 2))) || (i == 4 && ((j == 2) || (j == 4)))) {
+	for (i1 = 0; i1 < 3; i1++) {
+		for (j = 0; j < 5; j++) {
+			if (abs(j - 3 + 1) <= i1) {
+				printf("%c", a);
+			}
+			else {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	for (i2 = 3; i2 < 5; i2++) {
+		for (j = 0; j < 5; j++) {
+			if (abs(j - 3 + 1) <= (5 - (i2 + 1))) {
 				printf("%c", a);
 			}
 			else {
