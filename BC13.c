81876c8725bc4694631ec61b812ba97b57a8c0bd
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-05-01 23:31:54 +0800

    Update BC13.c

--- BC13.c
+++ BC13.c
@@ -26,3 +26,25 @@ int main() {
 	}
 	return 0;
 }
+
+//第二版，答案错误
+//考虑到循环中scanf可能获取不止一个数，改成getchar试试
+#include <stdio.h>
+
+int main() {
+	int b[8] = { 0 };
+	int i, y, m, d;
+	for (i = 0; i < 8; i++) {
+		b[i]=(int)getchar();
+	}
+	y = b[0] * 1000 + b[1] * 100 + b[2] * 10 + b[3];
+	m = b[4] * 10 + b[5];
+	d = b[6] * 10 + b[7];
+	if (y <= 2015 && y >= 1990 && m <= 12 && m >= 1 && d <= 30 && d >= 1) {
+		printf("year=%d\nmonth=%d%d\ndate=%d\n", y, b[4], b[5], d);
+	}
+	else {
+		printf("输入错误\n");
+	}
+	return 0;
+}
