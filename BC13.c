cb5b61bd88bdad49693731c51008b3c29032940c
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-05-02 13:21:34 +0800

    Update BC13.c

--- BC13.c
+++ BC13.c
@@ -65,3 +65,12 @@ int main() {
 //year = 50484951
 //month = 4850
 //date = 5053
+
+//第四版，答案正确
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%4d%2d%2d",&a,&b,&c);
+     printf("year=%d\nmonth=%02d\ndate=%02d\n",a,b,c);
+}
