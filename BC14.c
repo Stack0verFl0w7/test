41aacbbf6da0910ad039c835416ed6f60650e104
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-05-03 12:44:08 +0800

    Update BC14.c

--- BC14.c
+++ BC14.c
@@ -5,6 +5,7 @@
 //输出描述：
 //把两个整数按格式输出，中间用“,”分隔。
 
+//第一版
 #include <stdio.h>
 
 int main() {
@@ -13,6 +14,19 @@ int main() {
     i = a;
     a = b;
     b = i;
+    printf("a=%d,b=%d", a, b);
+    return 0;
+}
+
+//第二版
+#include <stdio.h>
+
+int main() {
+    int a, b, i;
+    scanf("a=%d,b=%d", &a, &b);
+    a = a + b;
+	b = a - b;
+	a = a - b;
     printf("a=%d,b=%d", a, b);
     return 0;
 }
