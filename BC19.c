b4f63e93835c2eb8cf47284d5b89d44930380c0f
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-05-11 22:44:06 +0800

    对齐

--- BC19.c
+++ BC19.c
@@ -5,12 +5,26 @@
 //输出描述：
 //输出 3 个整数以第二三个数字占 8 个空格靠右对齐输出
 
+//第一版
 #include <stdio.h>
 
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     printf("%-8d%-8d%d", a, b, c);
+    //%nd表示右对齐
+    //%-nd表示左对齐
+    return 0;
+}
+
+
+//第二版
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d%8d%8d", a, b, c);
     //%nd表示右对齐
     //%-nd表示左对齐
     return 0;
