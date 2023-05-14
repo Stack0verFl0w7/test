26017ff06f314570e858485a7e971d16f31c50f2
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-05-14 19:29:47 +0800

    加法

--- BC21.c
+++ BC21.c
@@ -5,6 +5,7 @@
 //输出描述：
 //输出一个整数.
 
+//一
 #include <stdio.h>
 
 int main() {
@@ -13,6 +14,20 @@ int main() {
     c=a+b;
     if(0<=a&&b<=1000){
         printf("%d",c);
+    }else{
+        printf("输入错误\n");
+    }
+    return 0;
+}
+
+//二
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d",&a,&b);
+    if(0<=a&&b<=1000){
+        printf("%d",a+b);
     }else{
         printf("输入错误\n");
     }
