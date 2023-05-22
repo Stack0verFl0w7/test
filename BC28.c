2985d422d3c2ec3f2cbe2d7c196ffb23d62b3add
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-05-22 23:12:35 +0800

    整数的十位

--- BC28.c
+++ BC28.c
@@ -5,11 +5,10 @@
 //输出描述：
 //输出一个整数
 
-#include <stdio.h>
-
-int main() {
+int main()
+{
     int a;
-    scanf("%d", &a);
-    printf("%d", (a - a % 10) % 100 / 10);
-    return 0;
+    scanf("%d",&a);
+    printf("%d",a%100/10);
+    return 0;   
 }
