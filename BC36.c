80c4e34ad69ae83aabb8f608b9400af07fbb8b00
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-05-31 22:50:06 +0800

    温度转换

--- BC36.c
+++ BC36.c
@@ -5,6 +5,7 @@
 //输出描述：
 //输出浮点数,保留3位小数
 
+//第一版，错误
 #include <stdio.h>
 
 int main() {
@@ -13,4 +14,19 @@ int main() {
     c = 5 / 9 * (f - 32);
     printf("%.3lf", c);
     return 0;
+}
+
+//第二版
+#include <stdio.h>
+
+int main() {
+    double f, c;
+    scanf("%lf", &f);
+    c = 5.0 / 9.0 * (f - 32);
+    printf("%.3lf", c);
+    return 0;
+//原式c=5/9*(f-32);
+//得到结果为0.000,
+//原因：整数相整除，不足1得0,0与其他相乘均为0
+//解决：讲5或9改为5.0或9.0
 }
