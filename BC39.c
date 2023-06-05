5b030b948fad5e4cc8c0e41cb0296cf9c5e56fbc
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-06-05 23:17:17 +0800

    水杯

--- BC39.c
+++ BC39.c
@@ -16,3 +16,22 @@ int main() {
     printf("%d", 10000 / b);
     return 0;
 }
+
+//第二版
+#include <stdio.h>
+
+int main()
+{
+    int h=0,r=0;
+    scanf("%d%d",&h,&r);
+    float v=h*3.14*r*r;
+    int c=10000;
+    int d=0;
+    while(c>0)
+    {
+        c=c-v;
+        d++;
+    }
+    printf("%d",d);
+    return 0;
+}
