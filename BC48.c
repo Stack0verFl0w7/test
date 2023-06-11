598ba3d3c931c231b9791b6aa11eb296770e9e94
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-06-11 17:04:35 +0800

    线段

--- BC48.c
+++ BC48.c
@@ -7,6 +7,7 @@
 //输出描述：
 //输出线段的长度的平方
 
+//第一版
 #include <stdio.h>
 
 int main() {
@@ -14,5 +15,21 @@ int main() {
     scanf("%d %d", &x1, &y1);
     scanf("%d %d", &x2, &y2);
     printf("%d", (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    return 0;
+}
+
+//第二版
+#include<stdio.h>
+#include<math.h>    //pow(x,y) 求x的y次方    //abs(x) 求x的绝对值
+int main(){
+    int x1,y1,x2,y2;
+    int r1,r2,r3,r4;
+    scanf("%d %d\n",&x1,&y1);
+    scanf("%d %d\n",&x2,&y2);
+    r1=abs(x1-x2);
+    r2=abs(y1-y2);
+    r3=pow(r1,2);
+    r4=pow(r2,2);
+    printf("%d",r3+r4);
     return 0;
 }
