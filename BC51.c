a47ed85b5bf5e92141ef7da35918101b244b00bc
Stack0verFl0w7 <2732226915@qq.com>
2023-06-14 22:22:12 +0800

    及格分数

---
+++ BC51.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+    while (scanf("%d", &score) != EOF) {
+        if (score >= 60) {
+            printf("Pass\n");
+        }
+        else {
+            printf("Fail\n");
+        }
+    }
+    return 0;
+}
