177f06282b08db74f5058d8430f8c54e9132065b
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-06-19 23:14:11 +0800

    闰年

--- BC55.c
+++ BC55.c
@@ -5,6 +5,7 @@
 //输出描述：
 ///是闰年输出"yes" 否则输出"no"
 
+//第一版
 #include <stdio.h>
 
 int main() {
@@ -23,6 +24,21 @@ int main() {
             printf("yes\n");
         }
         else {
+            printf("no\n");
+        }
+    }
+    return 0;
+}
+
+//第二版
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (n >= 1 && n <= 2018) {
+        if (n % 400 == 0 || (n % 4 == 0 && n % 100 != 0)){
+            printf("yes\n");
+        }
+        else{
             printf("no\n");
         }
     }
