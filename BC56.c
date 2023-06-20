e65c49125d5b7fa61c51afb009555f24fb52e5e8
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-06-20 15:00:10 +0800

    判断字母

--- BC56.c
+++ BC56.c
@@ -36,3 +36,15 @@ int main(){
 	}    
 	return 0;
 }
+
+//第三版
+#include <stdio.h>
+int main(){
+    char a;
+    scanf("%c",&a);
+    if(a>=65&&a<=90||a<=122&&a>=97)
+        printf("YES");
+    else
+        printf("NO");
+    return 0;
+}
