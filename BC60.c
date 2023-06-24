adba6ed9eb8a032964c12c3fcceffd964a7b8394
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-06-24 17:40:41 +0800

    判断是不是字母

--- BC60.c
+++ BC60.c
@@ -5,6 +5,7 @@
 //输出描述：
 //针对每组输入，输出单独占一行，判断输入字符是否为字母，输出内容详见输出样例。
 
+//第一版
 #include <stdio.h>
 
 int main() {
@@ -16,6 +17,23 @@ int main() {
         }
         else {
             printf("%c is not an alphabet.\n", c);
+        }
+    }
+    return 0;
+}
+
+//第二版
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a;
+    while((a=getchar())!= EOF)
+    {
+        if(a!='\n')    //主要判断是不是回车
+        {
+            if(a>='A'&&a<='z') printf("%c is an alphabet.\n",a);
+        else printf("%c is not an alphabet.\n",a);
         }
     }
     return 0;
