07930d0e80f8abff9dabcfa10ad2bd11de2bd283
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-06-30 21:15:57 +0800

    快递

--- BC64.c
+++ BC64.c
@@ -42,3 +42,41 @@ int main() {
     }
     return 0;
 }
+
+//第二版
+#include <stdio.h>
+
+int main() {
+    float a;
+    char b;
+    int c;
+    scanf("%f %c", &a, &b);
+    if (a <= 1) {
+        if (b == 'y') {
+            printf("25\n");
+        }
+        else {
+            printf("20\n");
+        }
+    }
+    if (a > 1) {
+        c = (int)a;
+        if (c != a ) {                              //更简便的方法
+            if (b == 'y') {
+                printf("%d", 20 + (c + 1 - 1) + 5);
+            }
+            else {
+                printf("%d", 20 + (c + 1 - 1));
+            }
+        }
+        else {
+            if (b == 'y') {
+                printf("%d", 20 + (c - 1) + 5);
+            }
+            else {
+                printf("%d", 20 + (c - 1));
+            }
+        }
+    }
+    return 0;
+}
