99f689bd23ccdad3adedd6b0ec1fc5c30c1386cc
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-07-09 22:06:11 +0800

    祝福

--- BC78.c
+++ BC78.c
@@ -1,3 +1,10 @@
+//描述
+//2023年来到了，KiKi要对他的n位好朋友说n遍”Happy new year!Good luck!”的祝福语。
+//输入描述：
+//输入只有一行，其中含有一个正整数 n(1≤n≤20)，代表KiKi说祝福话的遍数。
+//输出描述：
+//输出 n行"Happy new year!Good luck!"。
+
 #include<stdio.h>
 
 int main() {
