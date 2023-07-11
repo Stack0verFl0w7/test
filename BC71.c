4d453164f328c4b933b65fdcdf9547d63fb5be0f
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-07-11 23:27:00 +0800

    ？三角形

--- BC71.c
+++ BC71.c
@@ -5,6 +5,7 @@
 //输出描述：
 //针对每组输入数据，输出占一行，如果能构成三角形，等边三角形则输出“Equilateral triangle!”，等腰三角形则输出“Isosceles triangle!”，其余的三角形则输出“Ordinary triangle!”，反之输出“Not a triangle!”。
 
+//第一版
 #include <stdio.h>
 int main()
 {
@@ -33,6 +34,47 @@ int main()
         }
     }
     return 0;
+}
+
+//第二版，调用函数
+#include <stdio.h>
+
+void sanjiao(int chang, int kuan, int gao)
+//定义一个判断三角形的函数
+{
+	if ((chang < kuan + gao) && (kuan < chang + gao) && (gao < chang + kuan))
+	//判断是不是三角形
+	{
+		if ((chang == kuan) && (chang == gao))
+		//判断是不是等边三角形
+		{
+			printf("Equilateral triangle!\n");
+		}
+		else if ((chang == kuan) || (chang == gao) || (kuan == gao))
+		//判断是不是等腰三角形
+		{
+			printf("Isosceles triangle!\n");
+		}
+		else
+		//以上都不是，判断为普通三角形，执行下面的语句
+		{
+			printf("Ordinary triangle!\n");
+		}
+	}
+	else
+	//判断不是三角形的话，执行下面的语句
+	{
+		printf("Not a triangle!\n");
+	}
+}
+int main()
+{
+	int a, b, c;
+	while ((scanf("%d%d%d", &a, &b, &c)) != EOF)
+	{
+		sanjiao(a, b, c);
+	}
+	return 0;
 }
 
 
