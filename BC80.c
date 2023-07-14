8d96e17f9cbdfded35ee1b35b0c4dc39f7eb5e14
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-07-14 22:56:50 +0800

    奇偶统计

--- BC80.c
+++ BC80.c
@@ -5,6 +5,7 @@
 //输出描述：
 //一行，1~N之间奇数的个数和偶数的个数，用空格分开。
 
+//第一版
 #include <stdio.h>
 
 int main() {
@@ -20,6 +21,30 @@ int main() {
             Even++;
         }
     }
+    printf("%d %d",Odd,Even);
+    return 0;
+}
+
+//第二版，尝试写函数，但是耗时变长了，没啥用
+#include <stdio.h>
+int i;
+int N;
+int Odd=0;
+int Even=0;
+
+void count (int i,int N){
+    for(i=1;i<=N;i++){
+        if(i%2==1){
+            Odd++;
+        }else{
+            Even++;
+        }
+    }
+}
+
+int main() { 
+    scanf("%d",&N);
+    count(i,N);
     printf("%d %d",Odd,Even);
     return 0;
 }
