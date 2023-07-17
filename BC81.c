33c82505e80fed45f9f5903072c378d5ff113dc8
Stack0verFl0w7 <2732226915@qq.com>
2023-07-17 22:27:32 +0800

    求质数

---
+++ BC81.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n = 1;
+    int count = 0;
+    for (int i = 100; i <= 999; i++){
+        n = 1;
+        for (int j = 2; j < i; j++){
+            if (i % j == 0){
+                n = 0;
+            }
+        }
+        if (n){
+            count++;
+        }
+    }
+    printf("%d ", count);
+    return 0;
+}
