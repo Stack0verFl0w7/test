2c2b16873a53defc091bd52afd51142cbacc3a00
stackoverflow <127487417+Stack0verFl0w7@users.noreply.github.com>
2023-07-17 22:32:34 +0800

    求质数

--- BC81.c
+++ BC81.c
@@ -5,6 +5,7 @@
 //输出描述：
 //一行，一个整数，表示所有三位整数中，有多少个质数。
 
+//第一版
 #include<stdio.h>
 int main(){
     int n = 1;
@@ -21,5 +22,33 @@ int main(){
         }
     }
     printf("%d ", count);
+    return 0;
+}
+
+//第二版
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int x) {
+    if (x > 1) {
+        for (int i = 2; i * i <= x; ++i) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    int count = 0;
+    for(int i = 100; i < 1000; i++) {
+        if (isPrime(i)) {
+            count++;
+        }
+    }
+    printf("%d", count);
     return 0;
 }
