808b4fbd01932411d7f2f8dedf558a1548fa9121
Stack0verFl0w7 <2732226915@qq.com>
2023-08-02 23:26:39 +0800

    直角三角形图案

---
+++ BC100.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    while (scanf("%d", &n) != EOF)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int a = 0; a <= i; a++)
+                printf("* ");
+            printf("\n");
+        }
+    }
+    return 0;
+}
